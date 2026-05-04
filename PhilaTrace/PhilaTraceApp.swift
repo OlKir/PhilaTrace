d99333d7a5f953113fff0226c46e95ac11c4dadf
Oleksii Kirizii <oleksii.kirizii@gmail.com>
2026-05-04 11:26:13 +0200

    Initial Commit

---
+++ PhilaTrace/PhilaTraceApp.swift
@@ -0,0 +1,17 @@
+// PhilaTraceApp.swift
+// PhilaTrace
+//
+// Created by Oleksii Kirizii on 04.05.26.
+//
+
+
+import SwiftUI
+
+@main
+struct PhilaTraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhilaTraceTests/PhilaTraceTests.swift
@@ -0,0 +1,19 @@
+// PhilaTraceTests.swift
+// PhilaTrace
+//
+// Created by Oleksii Kirizii on 04.05.26.
+//
+
+
+import Testing
+@testable import PhilaTrace
+
+struct PhilaTraceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
